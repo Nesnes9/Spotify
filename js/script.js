a861be4cdaae47af15eb321e0fdd617587b469c2
Nesrine Tebaili <nesrine@MacBook-Air-de-Nesrine.local>
2020-01-30 20:58:24 +0100

    Ajout de la fonction qui assombrit le menu quand on scroll la page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-// fichier JS
+//Ecouter le scroll de la fenetre => header
+
+$(window).scroll(function(){
+	
+	var window_position = $(window).scrollTop(); // renvoie le nombre de pixel qui ont été scrollée dans l'élément
+	console.log(window_position);
+	
+	//Si la fenetre est scrollée  (scrollTop > 0) alors ajouter la classe .sticky
+	//Sinon retirer la classe . sticky
+	
+	if(window_position > 0){
+		
+		$('header').addClass('sticky');
+		console.log('ok');
+	}
+	
+	else{
+		
+		$('header').removeClass('sticky');
+		console.log('non');
+		
+	}
+	
+})
